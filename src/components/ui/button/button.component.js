481f115e5e5e6783300a61a0e304f436db8d0c1c
KomilovIslombek <islambek445@gmail.com>
2024-02-16 16:08:51 +0500

    added new methods for library

---
+++ src/components/ui/button/button.component.js
@@ -0,0 +1,29 @@
+import ChildComponent from '@/core/component/child.component'
+import renderService from '@/core/services/render.service'
+
+import styles from './button.module.scss'
+import template from './button.template.html'
+import { $I } from '@/core/iquery/iquery.lib'
+
+export class Button extends ChildComponent {
+	constructor({ children, onClick, variant }) {
+		super()
+
+		if (!children) throw new Error('Children is empty!')
+
+		this.children = children
+		this.onClick = onClick
+		this.variant = variant
+	}
+
+	render() {
+		this.element = renderService.htmlToElement(template, [], styles)
+
+		$I(this.element)
+			.html(this.children)
+			.click(this.onClick)
+			.addClass(styles[this.variant])
+
+		return this.element
+	}
+}
--- src/core/iquery/iquery.lib.js
+++ src/core/iquery/iquery.lib.js
@@ -17,6 +17,8 @@ export class IQuery {
 		}
 	}
 
+	/* FIND */
+
 	/**
 	 * Find the first element that matches the specified selector within the selected element.
 	 * @param {string|HTMLElement} selector - A CSS selector string to search for within the selected element.
@@ -32,41 +34,12 @@ export class IQuery {
 		}
 	}
 
-	/**
-	 * Get or set the inner HTML of the selected element.
-	 * @param {string} [htmlContent] - Optional HTML content to set. If
-	not provided, the current inner HTML will be returned
-	 * @returns {IQuery|string} The current IQuery instance for
-	chaining when setting HTML content, or the current inner HTML when getting.
-	 */
-	html(htmlContent) {
-		if (typeof htmlContent === 'undefined') {
-			return this.element.innerHTML
-		} else {
-			this.element.innerHTML = htmlContent
-			return this
-		}
-	}
-
-	/**
-	 * Set the CSS style of the selected element.
-	 * @param {string} property - The CSS property to set.
-	 * @param {string} value - The value to set for the CSS property.
-	 * @returns {IQuery} The current IQuery instance for chaining
-	 */
-	css(property, value) {
-		if (typeof property !== 'string' && typeof value !== 'string') {
-			throw new Error('property and value must be strings.')
-		}
-
-		this.element.style[property] = value
-		return this
-	}
+	/* INSERT */
 
 	/**
 	 * Append a new element as a child of the selected element.
 	 * @param {HTMLElement} childElement - The new child element to append.
-	 * @returns {IQuery} The current IQuery instance for chaining
+	 * @returns {IQuery} The current IQuery instance for chaining.
 	 */
 	append(childElement) {
 		if (!(childElement instanceof HTMLElement)) {
@@ -98,6 +71,118 @@ export class IQuery {
 	}
 
 	/**
+	 * Get or set the inner HTML of the selected element.
+	 * @param {string} [htmlContent] - Optional HTML content to set. If
+	not provided, the current inner HTML will be returned
+	 * @returns {IQuery|string} The current IQuery instance for
+	chaining when setting HTML content, or the current inner HTML when getting.
+	 */
+	html(htmlContent) {
+		if (typeof htmlContent === 'undefined') {
+			return this.element.innerHTML
+		} else {
+			this.element.innerHTML = htmlContent
+			return this
+		}
+	}
+
+	/* EVENTS */
+
+	/**
+	 * Attach a click event listener to the selected element.
+	 * @param {function(Event): void} callback - The event listener function to execute when the selected element is clicked. The function will receive the event object as its argument.
+	 * @returns {IQuery} The current IQuery instance for chaining.
+	 */
+	click(callback) {
+		this.element.addEventListener('click', callback)
+		return this
+	}
+
+	/* FORM */
+
+	/**
+	 * Set attributes and event listeners for an input element.
+	 * @param {object} options - An object containing input options.
+	 * @param {function(Event): void} [options.onInput] - The event listener for the input's input event.
+	 * @param {object} [options.rest] - Optional attributes to set on the input element.
+	 * @returns {IQuery} The current IQuery instance for chaining.
+	 */
+	input({ onInput, ...rest }) {
+		if (this.element.tagName.toLowerCase() !== 'input')
+			throw new Error('Element must be an input')
+
+		for (const [key, value] of Object.entries(rest)) {
+			this.element.setAttribute(key, value)
+		}
+
+		if (onInput) {
+			this.element.addEventListener('input', onInput)
+		}
+
+		return this
+	}
+
+	/**
+	 * Set attributes and event listeners for a number input element.
+	 * @param {number} [limit] - The maximum length of input value.
+	 * @returns {IQuery} The current IQuery instance for chaining.
+	 */
+	numberInput(limit) {
+		if (
+			this.element.tagName.toLowerCase() !== 'input' ||
+			this.element.type !== 'number'
+		)
+			throw new Error('Element must be an input with type "number"')
+
+		this.element.addEventListener('input', event => {
+			let value = event.target.value.replace(/[^0-9]/g, '')
+			if (limit) value = value.substring(0, limit)
+			event.target.value = value
+		})
+
+		return this
+	}
+
+	/**
+	 * Set attributes and event listeners for a credit card input element.
+	 * @returns {IQuery} The current IQuery instance for chaining.
+	 */
+	creditCardInput() {
+		const limit = 16
+
+		if (
+			this.element.tagName.toLowerCase() !== 'input' ||
+			this.element.type !== 'text'
+		)
+			throw new Error('Element must be an input with type "text"')
+
+		this.element.addEventListener('input', event => {
+			let value = event.target.value.replace(/[^0-9]/g, '')
+			if (limit) value = value.substring(0, limit)
+			event.target.value = formatCardNumberWithDashes(value)
+		})
+
+		return this
+	}
+
+	/* STYLES */
+
+	/**
+	 * Set the CSS style of the selected element.
+	 * @param {string} property - The CSS property to set.
+	 * @param {string} value - The value to set for the CSS property.
+	 * @returns {IQuery} The current IQuery instance for chaining
+	 */
+	css(property, value) {
+		if (typeof property !== 'string' && typeof value !== 'string') {
+			throw new Error('property and value must be strings.')
+		}
+
+		this.element.style[property] = value
+		return this
+	}
+
+	/**
 	 * Set the HTMLElement text of the selected element.
 	 * @param {string} text - The HTMLElement text to set.
 	 * @returns {IQuery} The current IQuery instance for chaining
@@ -108,6 +193,40 @@ export class IQuery {
 		}
 
 		this.element.textContent = text
+		return this
+	}
+
+	/**
+	 * Adds a class or a list of classes to the current element.
+	 * @param {string | string[]} classNames - A single class name or an array of class names to add to the element.
+	 * @returns {IQuery} The current IQuery instance for chaining.
+	 */
+	addClass(classNames) {
+		if (Array.isArray(classNames)) {
+			for (const className of classNames) {
+				this.element.classList.add(className)
+			}
+		} else {
+			this.element.classList.add(classNames)
+		}
+
+		return this
+	}
+
+	/**
+	 * Removes a class or a list of classes from the current element.
+	 * @param {string | string[]} classNames - A single class name or an array of class names to remove from the element.
+	 * @returns {IQuery} The current IQuery instance for chaining.
+	 */
+	removeClass(classNames) {
+		if (Array.isArray(classNames)) {
+			for (const className of classNames) {
+				this.element.classList.remove(className)
+			}
+		} else {
+			this.element.classList.remove(classNames)
+		}
+
 		return this
 	}
 }
